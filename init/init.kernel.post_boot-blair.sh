f33dde91ff78ce64330d397ce2876fe4393e5759
TogoFire <togofire@mailfence.com>
2024-07-12 04:22:37 -0300

    veux: init: up: Improve post_boot
    Change-Id: fe2c767966cb7aae575b93819b52283aaaa337bc
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- init/init.kernel.post_boot-blair.sh
+++ init/init.kernel.post_boot-blair.sh
@@ -1,204 +1,130 @@
 #=============================================================================
-# Copyright (c) 2021 Qualcomm Technologies, Inc.
+# Copyright (c) 2020-2021 Qualcomm Technologies, Inc.
 # All Rights Reserved.
 # Confidential and Proprietary - Qualcomm Technologies, Inc.
 #
-# Copyright (c) 2012-2013, 2016-2020, The Linux Foundation. All rights reserved.
+# Copyright (c) 2009-2012, 2014-2020, The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
+# modification, are permitted provided that the following conditions are met:
 #     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above
-#       copyright notice, this list of conditions and the following
-#       disclaimer in the documentation and/or other materials provided
-#       with the distribution.
-#     * Neither the name of The Linux Foundation nor the names of its
-#       contributors may be used to endorse or promote products derived
-#       from this software without specific prior written permission.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of The Linux Foundation nor
+#       the names of its contributors may be used to endorse or promote
+#       products derived from this software without specific prior written
+#       permission.
 #
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #=============================================================================
 
-function configure_zram_parameters() {
-	MemTotalStr=`cat /proc/meminfo | grep MemTotal`
-	MemTotal=${MemTotalStr:16:8}
+# Disable read-ahead swap
+echo 0 > /proc/sys/vm/page-cluster
 
-	# Zram disk - 75% for < 2GB devices .
-	# For >2GB devices, size = 50% of RAM size. Limit the size to 4GB.
+# configure input boost settings
+chown system:system /sys/devices/system/cpu/cpu_boost/input_boost_freq
+echo "0:1401600" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
+echo 200 > /sys/devices/system/cpu/cpu_boost/input_boost_ms
 
-	let RamSizeGB="( $MemTotal / 1048576 ) + 1"
-	diskSizeUnit=M
-	if [ $RamSizeGB -le 2 ]; then
-		let zRamSizeMB="( $RamSizeGB * 1024 ) * 3 / 4"
-	else
-		let zRamSizeMB="( $RamSizeGB * 1024 ) / 2"
-	fi
-
-	# use MB avoid 32 bit overflow
-	if [ $zRamSizeMB -gt 4096 ]; then
-		let zRamSizeMB=4096
-	fi
-
-	echo lz4 > /sys/block/zram0/comp_algorithm
-
-	if [ -f /sys/block/zram0/disksize ]; then
-		if [ -f /sys/block/zram0/use_dedup ]; then
-			echo 1 > /sys/block/zram0/use_dedup
-		fi
-		echo "$zRamSizeMB""$diskSizeUnit" > /sys/block/zram0/disksize
-
-		# ZRAM may use more memory than it saves if SLAB_STORE_USER
-		# debug option is enabled.
-		if [ -e /sys/kernel/slab/zs_handle ]; then
-			echo 0 > /sys/kernel/slab/zs_handle/store_user
-		fi
-		if [ -e /sys/kernel/slab/zspage ]; then
-			echo 0 > /sys/kernel/slab/zspage/store_user
-		fi
-
-		mkswap /dev/block/zram0
-		swapon /dev/block/zram0 -p 32758
-	fi
-}
-
-function configure_memory_parameters() {
-	MemTotalStr=`cat /proc/meminfo | grep MemTotal`
-	MemTotal=${MemTotalStr:16:8}
-	# Set Memory parameters.
-
-	# Set swappiness to 100 for all targets
-	#echo 100 > /proc/sys/vm/swappiness
-
-        # Set swappiness to 180 for all targets
-	echo 180 > /proc/sys/vm/swappiness
-
-	# Disable wsf for all targets beacause we are using efk.
-	# wsf Range : 1..1000 So set to bare minimum value 1.
-	echo 1 > /proc/sys/vm/watermark_scale_factor
-	# Disable the feature of watermark boost for 8G and below device
-	if [ $MemTotal -le 8388608 ]; then
-		echo 0 > /proc/sys/vm/watermark_boost_factor
-	fi
-	configure_zram_parameters
-
-	#Spawn 2 kswapd threads which can help in fast reclaiming of pages
-	echo 1 > /proc/sys/vm/kswapd_threads
-}
-
-# Core control parameters for silver
-echo 0 0 0 0 1 1 > /sys/devices/system/cpu/cpu0/core_ctl/not_preferred
-echo 4 > /sys/devices/system/cpu/cpu0/core_ctl/min_cpus
-echo 60 > /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres
-echo 40 > /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres
-echo 100 > /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms
-echo 8 > /sys/devices/system/cpu/cpu0/core_ctl/task_thres
-
-# Enable Core control for Silver
-echo 1 > /sys/devices/system/cpu/cpu0/core_ctl/enable
-
-# Disable Core control on gold
-echo 0 > /sys/devices/system/cpu/cpu6/core_ctl/enable
+#liochen@SYSTEM, 2020/11/02, Add for enable ufs performance
+echo 0 > /sys/class/scsi_host/host0/../../../clkscale_enable
 
 # Setting b.L scheduler parameters
-echo 65 > /proc/sys/kernel/sched_downmigrate
-echo 71 > /proc/sys/kernel/sched_upmigrate
-echo 85 > /proc/sys/kernel/sched_group_downmigrate
+echo 95 95 > /proc/sys/kernel/sched_upmigrate
+echo 85 85 > /proc/sys/kernel/sched_downmigrate
 echo 100 > /proc/sys/kernel/sched_group_upmigrate
+echo 85 > /proc/sys/kernel/sched_group_downmigrate
 echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
-echo 0 > /proc/sys/kernel/sched_coloc_busy_hysteresis_enable_cpus
-echo 0 > /proc/sys/kernel/sched_busy_hysteresis_enable_cpus
-echo 5 > /proc/sys/kernel/sched_ravg_window_nr_ticks
+echo 400000000 > /proc/sys/kernel/sched_coloc_downmigrate_ns
+echo 39000000 39000000 39000000 39000000 39000000 39000000 39000000 5000000 > /proc/sys/kernel/sched_coloc_busy_hyst_cpu_ns
+echo 240 > /proc/sys/kernel/sched_coloc_busy_hysteresis_enable_cpus
+echo 10 10 10 10 10 10 10 95 > /proc/sys/kernel/sched_coloc_busy_hyst_cpu_busy_pct
 
-# disable unfiltering
-echo 20000000 > /proc/sys/kernel/sched_task_unfilter_period
-
-# cpuset parameters
-echo 0-2     > /dev/cpuset/background/cpus
-echo 0-3     > /dev/cpuset/system-background/cpus
-echo 4-7     > /dev/cpuset/foreground/boost/cpus
-echo 0-2,4-7 > /dev/cpuset/foreground/cpus
-echo 0-7     > /dev/cpuset/top-app/cpus
-
-# Turn off scheduler boost at the end
-echo 0 > /proc/sys/kernel/sched_boost
+# set the threshold for low latency task boost feature which prioritize
+# binder activity tasks
+echo 325 > /proc/sys/kernel/walt_low_latency_task_threshold
+# change colocation threshold
+echo 162 > /proc/sys/kernel/sched_min_task_util_for_colocation
 
 # configure governor settings for silver cluster
 echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
-echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us
-echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/up_rate_limit_us
-echo 1113600 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/hispeed_freq
-echo 576000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
+echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/rate_limit_us
+echo 806400 > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
 
 # configure governor settings for gold cluster
-echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy6/scaling_governor
-echo 0 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/down_rate_limit_us
-echo 0 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/up_rate_limit_us
-echo 1228800 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/hispeed_freq
-echo 691200 > /sys/devices/system/cpu/cpufreq/policy6/scaling_min_freq
+echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy4/scaling_governor
+echo 0 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/rate_limit_us
 
-# Colocation V3 settings
-echo 680000 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/rtg_boost_freq
-echo 0 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/rtg_boost_freq
-echo 51 > /proc/sys/kernel/sched_min_task_util_for_boost
-echo 35 > /proc/sys/kernel/sched_min_task_util_for_colocation
+# configure governor settings for gold+ cluster
+echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy7/scaling_governor
+echo 0 > /sys/devices/system/cpu/cpufreq/policy7/schedutil/rate_limit_us
 
-# sched_load_boost as -6 is equivalent to target load as 85. It is per cpu tunable.
-echo -6 > /sys/devices/system/cpu/cpu6/sched_load_boost
-echo -6 > /sys/devices/system/cpu/cpu7/sched_load_boost
-echo 85 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/hispeed_load
-
-# configure input boost settings
-echo "0:1516800" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
-echo 120 > /sys/devices/system/cpu/cpu_boost/input_boost_ms
-echo "0:1804800 1:0 2:0 3:0 4:0 5:0 6:2208000 7:0" > /sys/devices/system/cpu/cpu_boost/powerkey_input_boost_freq
-echo 400 > /sys/devices/system/cpu/cpu_boost/powerkey_input_boost_ms
-
-# Enable bus-dcvs
-for device in /sys/devices/platform/soc
-do
-	for cpubw in $device/*cpu-cpu-ddr-bw/devfreq/*cpu-cpu-ddr-bw
+# configure bus-dcvs
+device=/sys/devices/platform/soc
+	for cpubw in $device/*cpu-cpu-llcc-bw/devfreq/*cpu-cpu-llcc-bw
 	do
 		cat $cpubw/available_frequencies | cut -d " " -f 1 > $cpubw/min_freq
-		echo "bw_hwmon" > $cpubw/governor
-		echo "762 1144 1720 2086 2597 2929 3879 5161 5931 6881 7980" > $cpubw/bw_hwmon/mbps_zones
+		echo "4577 7110 9155 12298 14236 15258" > $cpubw/bw_hwmon/mbps_zones
 		echo 4 > $cpubw/bw_hwmon/sample_ms
-		echo 68 > $cpubw/bw_hwmon/io_percent
+		echo 80 > $cpubw/bw_hwmon/io_percent
 		echo 20 > $cpubw/bw_hwmon/hist_memory
-		echo 0 > $cpubw/bw_hwmon/hyst_length
-		echo 80 > $cpubw/bw_hwmon/down_thres
+		echo 10 > $cpubw/bw_hwmon/hyst_length
+		echo 30 > $cpubw/bw_hwmon/down_thres
 		echo 0 > $cpubw/bw_hwmon/guard_band_mbps
 		echo 250 > $cpubw/bw_hwmon/up_scale
 		echo 1600 > $cpubw/bw_hwmon/idle_mbps
+		echo 12298 > $cpubw/max_freq
 		echo 40 > $cpubw/polling_interval
 	done
 
-	# configure compute settings for silver latfloor
-	for latfloor in $device/*cpu0-cpu*latfloor/devfreq/*cpu0-cpu*latfloor
+	for llccbw in $device/*cpu-llcc-ddr-bw/devfreq/*cpu-llcc-ddr-bw
 	do
-		cat $latfloor/available_frequencies | cut -d " " -f 1 > $latfloor/min_freq
-		echo 8 > $latfloor/polling_interval
+		cat $llccbw/available_frequencies | cut -d " " -f 1 > $llccbw/min_freq
+		if [ ${ddr_type:4:2} == $ddr_type4 ]; then
+		echo "1720 2086 2929 3879 5931 6515 8136" > $llccbw/bw_hwmon/mbps_zones
+		elif [ ${ddr_type:4:2} == $ddr_type5 ]; then
+		echo "1720 2086 2929 3879 6515 7980 12191" > $llccbw/bw_hwmon/mbps_zones
+		fi
+		echo 4 > $llccbw/bw_hwmon/sample_ms
+		echo 80 > $llccbw/bw_hwmon/io_percent
+		echo 20 > $llccbw/bw_hwmon/hist_memory
+		echo 10 > $llccbw/bw_hwmon/hyst_length
+		echo 30 > $llccbw/bw_hwmon/down_thres
+		echo 0 > $llccbw/bw_hwmon/guard_band_mbps
+		echo 250 > $llccbw/bw_hwmon/up_scale
+		echo 1600 > $llccbw/bw_hwmon/idle_mbps
+		echo 6515 > $llccbw/max_freq
+		echo 40 > $llccbw/polling_interval
 	done
 
-	# configure compute settings for gold latfloor
-	for latfloor in $device/*cpu6-cpu*latfloor/devfreq/*cpu6-cpu*latfloor
+	for l3bw in $device/*snoop-l3-bw/devfreq/*snoop-l3-bw
 	do
-		cat $latfloor/available_frequencies | cut -d " " -f 1 > $latfloor/min_freq
-		echo 8 > $latfloor/polling_interval
+		cat $l3bw/available_frequencies | cut -d " " -f 1 > $l3bw/min_freq
+		echo 4 > $l3bw/bw_hwmon/sample_ms
+		echo 10 > $l3bw/bw_hwmon/io_percent
+		echo 20 > $l3bw/bw_hwmon/hist_memory
+		echo 10 > $l3bw/bw_hwmon/hyst_length
+		echo 0 > $l3bw/bw_hwmon/down_thres
+		echo 0 > $l3bw/bw_hwmon/guard_band_mbps
+		echo 0 > $l3bw/bw_hwmon/up_scale
+		echo 1600 > $l3bw/bw_hwmon/idle_mbps
+		echo 9155 > $l3bw/max_freq
+		echo 40 > $l3bw/polling_interval
 	done
 
-	# configure mem_latency settings for DDR scaling
+	# configure mem_latency settings for LLCC and DDR scaling and qoslat
 	for memlat in $device/*lat/devfreq/*lat
 	do
 		cat $memlat/available_frequencies | cut -d " " -f 1 > $memlat/min_freq
@@ -206,26 +132,71 @@ do
 		echo 400 > $memlat/mem_latency/ratio_ceil
 	done
 
-	#Gold CPU6 L3 ratio ceil
+	# configure compute settings for gold latfloor
+	for latfloor in $device/*cpu4-cpu*latfloor/devfreq/*cpu4-cpu*latfloor
+	do
+		cat $latfloor/available_frequencies | cut -d " " -f 1 > $latfloor/min_freq
+		echo 8 > $latfloor/polling_interval
+	done
+
+	# configure mem_latency settings for prime latfloor
+	for latfloor in $device/*cpu7-cpu*latfloor/devfreq/*cpu7-cpu*latfloor
+	do
+		cat $latfloor/available_frequencies | cut -d " " -f 1 > $latfloor/min_freq
+		echo 8 > $latfloor/polling_interval
+		echo 25000 > $latfloor/mem_latency/ratio_ceil
+	done
+
+	# CPU4 L3 ratio ceil
+	for l3gold in $device/*cpu4-cpu-l3-lat/devfreq/*cpu4-cpu-l3-lat
+	do
+		echo 4000 > $l3gold/mem_latency/ratio_ceil
+	done
+
+	# CPU5 L3 ratio ceil
+	for l3gold in $device/*cpu5-cpu-l3-lat/devfreq/*cpu5-cpu-l3-lat
+	do
+		echo 4000 > $l3gold/mem_latency/ratio_ceil
+	done
+
+	# CPU6 L3 ratio ceil
 	for l3gold in $device/*cpu6-cpu-l3-lat/devfreq/*cpu6-cpu-l3-lat
 	do
 		echo 4000 > $l3gold/mem_latency/ratio_ceil
-		echo 25000 > $l3gold/mem_latency/wb_filter_ratio
-		echo 60 > $l3gold/mem_latency/wb_pct_thres
 	done
 
-	#Gold CPU7 L3 ratio ceil
-	for l3gold in $device/*cpu7-cpu-l3-lat/devfreq/*cpu7-cpu-l3-lat
+	# prime L3 ratio ceil
+	for l3prime in $device/*cpu7-cpu-l3-lat/devfreq/*cpu7-cpu-l3-lat
 	do
-		echo 4000 > $l3gold/mem_latency/ratio_ceil
-		echo 25000 > $l3gold/mem_latency/wb_filter_ratio
-		echo 60 > $l3gold/mem_latency/wb_pct_thres
+		echo 20000 > $l3prime/mem_latency/ratio_ceil
 	done
 
-done
+	# qoslat ratio ceil
+	for qoslat in $device/*qoslat/devfreq/*qoslat
+	do
+		echo 50 > $qoslat/mem_latency/ratio_ceil
+	done
 
 echo N > /sys/module/lpm_levels/parameters/sleep_disabled
 
-configure_memory_parameters
+# Remove unused swapfile
+rm -rf /data/vendor/swap
+sync
+
+# Let kernel know our image version/variant/crm_version
+if [ -f /sys/devices/soc0/select_image ]; then
+	image_version="10:"
+	image_version+=`getprop ro.build.id`
+	image_version+=":"
+	image_version+=`getprop ro.build.version.incremental`
+	image_variant=`getprop ro.product.name`
+	image_variant+="-"
+	image_variant+=`getprop ro.build.type`
+	oem_version=`getprop ro.build.version.codename`
+	echo 10 > /sys/devices/soc0/select_image
+	echo $image_version > /sys/devices/soc0/image_version
+	echo $image_variant > /sys/devices/soc0/image_variant
+	echo $oem_version > /sys/devices/soc0/image_crm_version
+fi
 
 setprop vendor.post_boot.parsed 1
