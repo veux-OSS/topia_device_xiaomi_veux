ca7eb213b158ce5860fb64cb9807201388bb0349
kenway214 <kenway214@outlook.com>
2024-11-24 11:16:17 +0530

    veux: parts: Introduce Touch boost (HTSR) tile service
    - Introduced `TouchSamplingTileService` to provide a Quick Settings tile for enabling/disabling touch boost functionality.
    - Added logic to toggle touch sampling state and persist the state using SharedPreferences.
    - Integrated file system updates to apply touch sampling changes dynamically.
    - Included a `BootCompletedReceiver` to reinitialize the touch sampling state after boot.
    
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>
    Signed-off-by: MrTopia <redkarmakar@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -26,6 +26,7 @@ import android.util.Log;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.touchsampling.TouchSamplingUtils;
 import org.lineageos.settings.touchsampling.TouchSamplingService;
+import org.lineageos.settings.touchsampling.TouchSamplingTileService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
@@ -54,6 +55,10 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         // Start Touch Sampling Service
         context.startServiceAsUser(new Intent(context, TouchSamplingService.class),
+                UserHandle.CURRENT);
+
+        // Touch Sampling Tile Service
+        context.startServiceAsUser(new Intent(context, TouchSamplingTileService.class), 
                 UserHandle.CURRENT);
     }
 }
