648470b586618d6990cbd99f58cff0cb62d35349
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    veux: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -244,6 +244,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/overlay/config-vendor.xml:$(TARGET_COPY_OUT_VENDOR)/overlay/config/config.xml
 
 # Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Perf
